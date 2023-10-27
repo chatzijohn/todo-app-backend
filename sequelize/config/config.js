db413c52745160f78a70010c67c2296f4c9b1b07
pchatzigiannis <pchatzigiannis@technor.gr>
2023-10-27 09:57:41 +0300

    Changed Timezone

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -9,21 +9,33 @@ const sequelizeConfig = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      useUTC: false, // for reading from database
+    },
+    timezone: '+03:00', // for writing to database
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      useUTC: false, // for reading from database
+    },
+    timezone: '+03:00', // for writing to database
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      useUTC: false, // for reading from database
+    },
+    timezone: '+03:00', // for writing to database
   }
 }
 
